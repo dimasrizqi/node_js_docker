1065a15986de624e3fe3186d15097c34feb7f098
dimasrizqi <dimasrizqi3128@yahoo.co.id>
2022-07-29 10:12:17 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require(‘express’);
+
+var app = express();
+
+ 
+
+app.get(‘/’, function (req, res) {
+
+res.send(‘Hi, this is NodeJS Dewiweb!’);
+
+});
+
+ 
+
+app.listen(3000, function () {
+
+console.log(‘apps running under port 3000!’);
+
+});
